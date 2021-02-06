91ad737fa86d9a0a62df6fca51746c163a566120
Dinh Hoang Nhi <nhidh99@gmail.com>
2021-02-07 03:07:40 +0700

    Remove unused files and update security

--- laptop-store-backend/security/src/main/kotlin/org/example/security/WebSecurityConfig.kt
+++ laptop-store-backend/security/src/main/kotlin/org/example/security/WebSecurityConfig.kt
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import java.lang.Exception
 
 @Configuration
 @EnableWebSecurity
@@ -42,6 +41,6 @@ class WebSecurityConfig @Autowired constructor(
     @Bean
     @Throws(Exception::class)
     override fun authenticationManagerBean(): AuthenticationManager {
-        return super.authenticationManagerBean()
+        return authenticationManager()
     }
 }
--- laptop-store-backend/src/main/kotlin/org/example/LaptopStoreApplication.kt
+++ laptop-store-backend/src/main/kotlin/org/example/LaptopStoreApplication.kt
@@ -1,4 +1,4 @@
-package org.example.application
+package org.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
